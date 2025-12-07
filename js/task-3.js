b51e58ba570de6cd5fd0f433a99fe1bf341f1452
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-12-07 20:15:13 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,7 @@
 const userNameInput = document.getElementById("name-input");
-
 const userNameOutput = document.getElementById("name-output");
 
 userNameInput.addEventListener("input", handleOutputUserName);
-
-console.log(userNameInput);
 
 function handleOutputUserName(e) {
   let userName = e.currentTarget.value.trim();
