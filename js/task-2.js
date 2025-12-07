33e38f71cc24373b20ccafbe51bd21c3f8b7c3c2
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-12-07 17:51:16 +0200

    Complete task-2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,14 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+images.forEach((image) => {
+  galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li><img src="${image.url}" alt="${image.alt}" class="gallery-image"></li>`
+  );
+});
