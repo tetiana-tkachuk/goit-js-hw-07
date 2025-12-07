f5319a1804573f517ba39fa600b58bb9e9865235
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2025-12-07 20:45:59 +0200

    Complete task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", changeBodyColor);
+
+function changeBodyColor(e) {
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+
+  colorName.textContent = newColor;
+}
